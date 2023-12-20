5d1643ff11650375ce0772a3510333e9513824f3
xingguangzfs <fushou3579@163.com>
2023-12-21 07:12:53 +0800

    1

--- src/spiderset/toutiao/toutiao/spiders/toutiao_suggest.py
+++ src/spiderset/toutiao/toutiao/spiders/toutiao_suggest.py
@@ -13,13 +13,19 @@ from toutiao.common import item_toutiao_suggest_response_text_key
 class ToutiaoSuggestSpider(scrapy.Spider):
     name = "toutiao_suggest"
     allowed_domains = ["www.toutiao.com"]
-    start_urls = ["https://www.toutiao.com"]
+    start_urls = ["https://www.toutiao.com/ch/news_hot/"]
 
     def parse(self, response):
         #pass
         item = ToutiaoItem()
 
         item[item_toutiao_suggest_response_text_key] = response.text
+
+        for news in response.css('.title-box'):
+            title = news.css('.title::text').extract_first()
+            url = news.css('.link::attr(href)').extract_first()
+
+            print('item title:' + title + ", url:" + url)
 
         # 提交item到管道文件
         yield item
